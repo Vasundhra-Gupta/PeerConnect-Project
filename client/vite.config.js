0da78ff33aafe1dabf4820660a1049d1f93c4b03
Sania Singla <singlasania7717@gmail.com>
2025-05-26 11:54:25 +0530

    basic structure for the project

---
+++ client/vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: './src/Config/.env' }); // no need if in root dir
+
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            'codemirror/lib/codemirror.css': 'codemirror/lib/codemirror.css',
+            'codemirror/mode/javascript/javascript':
+                'codemirror/mode/javascript/javascript.js',
+            'codemirror/addon/edit/closetag':
+                'codemirror/addon/edit/closetag.js',
+            'codemirror/addon/edit/closebrackets':
+                'codemirror/addon/edit/closebrackets.js',
+            'codemirror/theme/dracula.css': 'codemirror/theme/dracula.css',
+        },
+    },
+});
